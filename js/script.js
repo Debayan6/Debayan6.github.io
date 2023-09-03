21f6fd0bd858efa6f4164ac515a6391cb7bd9a5e
Debayan6 <134131521+Debayan6@users.noreply.github.com>
2023-09-03 16:59:21 +0530

    brief change  script.js

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ window.onscroll = () => {
 // });
 
 var typed = new Typed('.multiple-text', {
-  strings: ['a Learner.', 'a Cosmophile.', 'stardust.'],
+  strings: ['a Cosmophile.', 'stardust.'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
